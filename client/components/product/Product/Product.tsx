e40678e3e058b3536a6eb16f43c5fd345ed7643a
IlyaVorWork <77672212+IlyaVorWork@users.noreply.github.com>
2021-05-11 19:33:54 +0700

    initial commit

--- client/components/product/Product/Product.tsx
+++ client/components/product/Product/Product.tsx
@@ -23,12 +23,14 @@ import { useMutation } from '@apollo/client'
 import { AppContext } from '@providers/AppProvider'
 import { makeBid, newPrice } from '@utils/account'
 import UPDATE_PRICE from '@graphql/mutations/UpdatePrice'
+import { useRouter } from 'next/router'
 
 interface IProductComponent {
   product: IProductProps
 }
 
 const Product: FunctionComponent<IProductComponent> = ({ product }) => {
+  const router = useRouter()
   const classes = useStyles()
   const { state, dispatch } = useContext(ShopContext)
   const { state: appState, dispatch: appDispatch } = useContext(AppContext)
@@ -123,7 +125,7 @@ const Product: FunctionComponent<IProductComponent> = ({ product }) => {
         enqueueSnackbar('Ставка принята, спасибо за ожидание!', {
           variant: 'success',
         })
-        location.reload()
+        router.push('/my-account?panel=6')
       }
     }
   }
